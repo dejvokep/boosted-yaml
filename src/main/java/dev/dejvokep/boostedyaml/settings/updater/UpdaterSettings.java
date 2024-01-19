5d983299de90a79c0f4f5169351261d00d81637b
dejvokep <davidcubesvkdev@gmail.com>
2024-01-19 18:17:44 +0100

    Updated license year.

--- src/main/java/dev/dejvokep/boostedyaml/settings/updater/UpdaterSettings.java
+++ src/main/java/dev/dejvokep/boostedyaml/settings/updater/UpdaterSettings.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 https://dejvokep.dev/
+ * Copyright 2024 https://dejvokep.dev/
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
