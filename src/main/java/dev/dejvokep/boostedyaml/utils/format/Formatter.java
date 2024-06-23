374481d32a92580304a3d4b45954003d77a5db70
dejvokep <37455929+dejvokep@users.noreply.github.com>
2024-06-23 23:23:06 +0100

    Annotate the formatter methods

--- src/main/java/dev/dejvokep/boostedyaml/utils/format/Formatter.java
+++ src/main/java/dev/dejvokep/boostedyaml/utils/format/Formatter.java
@@ -1,11 +1,14 @@
 package dev.dejvokep.boostedyaml.utils.format;
 
+import org.jetbrains.annotations.NotNull;
 import org.snakeyaml.engine.v2.nodes.Tag;
 
 public interface Formatter<S, V> {
 
-    S format(Tag tag, V value, NodeRole role, S previous);
+    @NotNull
+    S format(@NotNull Tag tag, @NotNull V value, @NotNull NodeRole role, @NotNull S previous);
 
+    @NotNull
     static <S, V> Formatter<S, V> identity() {
         return (tag, value, role, previous) -> previous;
     }
