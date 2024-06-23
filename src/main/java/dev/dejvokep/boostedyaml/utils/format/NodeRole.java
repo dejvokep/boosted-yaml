4d688b9e5473dc984bfc744fce0d57b54a1a1d1e
dejvokep <37455929+dejvokep@users.noreply.github.com>
2024-06-23 23:04:04 +0100

    Abstract node role

---
+++ src/main/java/dev/dejvokep/boostedyaml/utils/format/NodeRole.java
@@ -0,0 +1,15 @@
+package dev.dejvokep.boostedyaml.utils.format;
+
+/**
+ * Represents the role of the node.
+ */
+public enum NodeRole {
+    /**
+     * The node is a key of a mapping.
+     */
+    KEY,
+    /**
+     * The node is a value of a mapping.
+     */
+    VALUE
+}
